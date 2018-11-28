7f76ddcd8f063f5aac8b0eac239eeca210c89957
roxas_huang <roxas_huang@friday.tw>
2018-11-28 10:03:47 +0800

    使用 puppetter 作為爬蟲工具

--- app.js
+++ app.js
@@ -1,29 +1,34 @@
-const HCCrawler = require('headless-chrome-crawler')
+const puppeteer = require("puppeteer")
+const cheerio = require('cheerio')
 
 const targets = [
-    {
-        name: 'PChome 購物中心 熱門關鍵字',
-        url: 'https://mall.pchome.com.tw/',
-        query: '#site_nav1 .body ul li a'
-    },
-    {
-        name: 'Yahoo 購物中心 熱門關鍵字',
-        url: 'https://tw.buy.yahoo.com/',
-        query: '.hotSearch a'
-    }
+  {
+      name: 'PChome 購物中心 熱門關鍵字',
+      url: 'https://mall.pchome.com.tw/',
+      query: '#site_nav1 .body ul li a'
+  },
+  {
+      name: 'Yahoo 購物中心 熱門關鍵字',
+      url: 'https://tw.buy.yahoo.com/generic/ajax/adModule?moduleId=d_hotsearch',
+      query: '.hotSearch a'
+  }
 ]
 
-const getKeyWordA = async ({url, query}) => {
-    const crawler = await HCCrawler.launch({
-        evaluatePage: () => Object.values($('.hotSearch a')).map((el, idx) => $('.hotSearch a').eq(idx).text()),
-        onSuccess: result => {  
-            console.log('result :', result.result);
-        },
-    });
-    // Queue a request
-    await crawler.queue(url)
-    await crawler.onIdle()
-    await crawler.close()
+const runBrowser = async ({url, query}) => {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+
+  page.on('load', async () => {
+
+    const aHandle = await page.evaluateHandle(() => document.body);
+    const resultHandle = await page.evaluateHandle(body => body.innerHTML, aHandle);
+    console.log(await resultHandle.jsonValue());
+    await resultHandle.dispose()
+
+    // await browser.close()
+  })
+  await page.goto(url)
 }
 
-getKeyWordA(targets[1])
+runBrowser(targets[0])
+// runBrowser(targets[1])
