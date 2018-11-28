4770b8667d50bb99d6cfa61dcf885889a66a46af
roxas_huang <roxas_huang@friday.tw>
2018-11-28 11:11:32 +0800

    取得 pchome 的 DOM 元素

--- app.js
+++ app.js
@@ -14,21 +14,21 @@ const targets = [
   }
 ]
 
-const runBrowser = async ({url, query}) => {
+const runBrowser = async ({url, query, name}) => {
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
-
-  page.on('load', async () => {
-
-    const aHandle = await page.evaluateHandle(() => document.body);
-    const resultHandle = await page.evaluateHandle(body => body.innerHTML, aHandle);
-    console.log(await resultHandle.jsonValue());
-    await resultHandle.dispose()
-
-    // await browser.close()
+  await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36');
+  await page.setExtraHTTPHeaders({
+    'accept-language': 'en-US,en;q=0.8'
   })
-  await page.goto(url)
+  await page.goto(url) 
+  await page.waitForSelector(query)
+  const result = await page.evaluate((q) => {
+    return document.querySelectorAll(q).length
+  }, query)
+  await browser.close()
+  console.log('result :', result, name);
 }
 
 runBrowser(targets[0])
-// runBrowser(targets[1])
+runBrowser(targets[1])
